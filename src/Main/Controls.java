5161e6dd85b83c5d2b6e2f43966456ce1f2fde34
Crehop <crehop2@gmail.com>
2014-10-20 11:00:02 -0700

    Restructured Control interface
    Switched controls to boolean for easier use.

--- src/Main/Controls.java
+++ src/Main/Controls.java
@@ -1,14 +1,26 @@
 package Main;
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 
 import static org.lwjgl.input.Keyboard.*;
 
 public class Controls {
-
+	
 	public static void checkInput() {
+		//Set check key boolean, then add if statement below to actuate
+		boolean exitKey = Keyboard.isKeyDown(KEY_ESCAPE);
+		boolean forward = Keyboard.isKeyDown(KEY_W);
+		boolean back = Keyboard.isKeyDown(KEY_S);
+		boolean strafeLeft = Keyboard.isKeyDown(KEY_A);
+		boolean strafeRight = Keyboard.isKeyDown(KEY_D);
+		boolean jump = Keyboard.isKeyDown(KEY_SPACE);
+		boolean crouch = Keyboard.isKeyDown(KEY_LSHIFT);
+		boolean stabalizeCam = Keyboard.isKeyDown(KEY_TAB);
+		boolean rotateLeft = Keyboard.isKeyDown(KEY_Q);
+		boolean rotateRight = Keyboard.isKeyDown(KEY_E);
 		boolean aKeyIsDown = false;
-		if(Keyboard.isKeyDown(KEY_ESCAPE)){
+		if(exitKey){
 			Main.scrub();
 			aKeyIsDown = true;
 		}
@@ -16,39 +28,39 @@ public class Controls {
 			Main.setLoop("game");
 			aKeyIsDown = true;
 		}
-		if(Keyboard.isKeyDown(KEY_W)){
+		if(forward){
 			Main.cam.setZ(Main.cam.getZ() + 0.01f);
 			aKeyIsDown = true;
 		}
-		if(Keyboard.isKeyDown(KEY_S)){
+		if(back){
 			Main.cam.setZ(Main.cam.getZ() - 0.01f);
 			aKeyIsDown = true;
 		}
-		if(Keyboard.isKeyDown(KEY_A)){
+		if(strafeLeft){
 			Main.cam.setX(Main.cam.getX() + 0.01f);
 			aKeyIsDown = true;
 		}
-		if(Keyboard.isKeyDown(KEY_D)){
+		if(strafeRight){
 			Main.cam.setX(Main.cam.getX() - 0.01f);		
 			aKeyIsDown = true;
 		}
-		if(Keyboard.isKeyDown(KEY_SPACE)){
+		if(jump){
 			Main.cam.setY(Main.cam.getY() - 0.01f);
 			aKeyIsDown = true;
 		}
-		if(Keyboard.isKeyDown(KEY_LSHIFT)){
+		if(crouch){
 			Main.cam.setY(Main.cam.getY() + 0.01f);
 			aKeyIsDown = true;
 		}
-		if(Keyboard.isKeyDown(KEY_TAB)){
+		if(stabalizeCam){
 			Main.cam.stabalize();
 			aKeyIsDown = true;
 		}
-		if(Keyboard.isKeyDown(KEY_Q)){
+		if(rotateLeft){
 			Main.cam.setRY(Main.cam.getRY() - 0.01f);
 			aKeyIsDown = true;
 		}
-		if(Keyboard.isKeyDown(KEY_E)){
+		if(rotateRight){
 			Main.cam.setRY(Main.cam.getRY() + 0.01f);
 			aKeyIsDown = true;
 		}
