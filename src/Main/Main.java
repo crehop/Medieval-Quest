76730e55ee57b6866dd2e1d924f62308b883f53d
Crehop <crehop2@gmail.com>
2014-12-05 14:16:47 -0800

    keeping on keepin on

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -48,7 +48,6 @@ public class Main {
 			if(loopSwitch == true){
 				StartLoop.toggleInitiate();
 				loopSwitch = false;
-				System.out.println("loopSwitch = true");
 			}
 			StartLoop.loop();
 			isInGameMode = false;
--- src/loops/GameLoop.java
+++ src/loops/GameLoop.java
@@ -1,14 +1,6 @@
 package loops;
  
-import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
-import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
-import static org.lwjgl.opengl.GL11.GL_PROJECTION;
-import static org.lwjgl.opengl.GL11.glClear;
-import static org.lwjgl.opengl.GL11.glEnable;
-import static org.lwjgl.opengl.GL11.glLoadIdentity;
-import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.util.glu.GLU.gluPerspective;
 
 import java.util.ArrayList;
@@ -34,7 +26,7 @@ public class GameLoop {
 	static Random rand = new Random();
 	static boolean initiated = false;
 	static Texture grass = renderer.TextureHandler.getTexture("grass");
-	static Voxel test = new Voxel(new Location(0,0,0), 10.0f, grass, 0, 0, 0);
+	static Voxel test = new Voxel(new Location(10,10,10), 10.0f, grass, 0.0f, 0.0f, 0.0f);
 	public static void loop(){
 		if(!(initiated)){
 			initProjection();
@@ -43,19 +35,19 @@ public class GameLoop {
 		if(Mouse.isGrabbed() == false){
 			Mouse.setGrabbed(true);
 		}
-		glLoadIdentity();
 		Main.cam.useView();
-		Controls.checkInput();
-		Driver.checkForTick();
 		Skybox.renderSkyBox(Main.cam);
 		test.render();
-		//System.out.println(Main.cam.getLocation().getX() + " " + Main.cam.getLocation().getY() + " " + Main.cam.getLocation().getZ() );
+		Controls.checkInput();
+		//Driver.checkForTick();
+		System.out.println(Main.cam.getLocation().getX() + " " + Main.cam.getLocation().getY() + " " + Main.cam.getLocation().getZ() );
 	}
 	private static void initProjection() {
+		glEnd();
 		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
 		gluPerspective(70,(float)Display.getWidth()/(float)Display.getHeight(),0.0003f,1000);
 		glMatrixMode(GL_MODELVIEW);
-		glEnable(GL_DEPTH_TEST);
 	}
 	public static void toggleInitiate(){
 		if(initiated == true){
