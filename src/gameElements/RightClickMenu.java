c9ae0db40291b263b11db44be217cf8f87b1f45b
Crehop <crehop2@gmail.com>
2015-01-18 00:06:58 -0800

    Continued prep
    continued right click menu prep

--- src/gameElements/RightClickMenu.java
+++ src/gameElements/RightClickMenu.java
@@ -1,18 +1,32 @@
 package gameElements;
-
-import java.util.ArrayList;
-
 import org.newdawn.slick.opengl.Texture;
-
 import utils.TextureHandler;
 
 public class RightClickMenu {
-	public static Texture menu = null;
+	public static Texture menu = TextureHandler.getTexture("res/textures/menuitem.png");
 	public static int x = 0;
 	public static int y = 0;
-	public static String[] lines = new String[11] ;
+	private static int choice = 0;
+	public static boolean menuOpen = false;
+	public static boolean clicked = false;
+	public static int[] xy = {0,43,44,85,86,128,129,171,25,475};
 	
-	public static extureHandler.getTexture("res/models/wall/wall.obj") displayMenu(){
+	
+	public static void openMenu(String[] lines, Object obj){
+		if(menuOpen){
+			closeMenu();
+		}
+	}
+	
+	public static int getChoice(){
+		return choice;
+	}
+	public static void closeMenu(){
 		
+	}
+	public static void render(){
+		if(menuOpen){
+			
+		}
 	}
 }
